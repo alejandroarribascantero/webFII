f9d4da67ce3dea04707956924cb184b7716d0d09
alejandro <alejandroarribascantero@gmail.com>
2023-11-28 11:40:56 +0100

    Añadir barra de navegacion. footer e inicio de formulario para crear una noticia (Hacer estilo paa que aparezca en el centro de la pantalla)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Agregar evento clic al botón utilizando jQuery
+    $('#btnAbrirFormulario').click(function () {
+        // Mostrar u ocultar el formulario utilizando jQuery
+        $('#noticiaForm').toggle();
+    });
+});
